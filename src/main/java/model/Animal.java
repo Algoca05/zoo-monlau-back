4ffe3dd588e38d9f70612bc924c5ab27dfa3edbc
Algoca05 <alejandrogomcap@campus.monlau.com>
2025-04-01 17:44:55 +0200

    qwerty

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -1,4 +1,25 @@
 package model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "animales")
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String especie;
+    private Integer salud;
+    @ManyToOne
+    @JoinColumn(name = "empleado_id")
+    private Empleado empleado;
+
 }
--- src/main/java/model/Empleado.java
+++ src/main/java/model/Empleado.java
@@ -1,4 +1,19 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data@AllArgsConstructor@NoArgsConstructor
+@Entity
+@Table(name = "empleados")
 public class Empleado {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    @OneToMany(mappedBy = "empleado", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Animal> animales;
 }
